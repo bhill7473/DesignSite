e19b84284897c6e9b39db5c1a8b5dace0ff694f9
bhill7473 <bhill7473@gmail.com>
2018-04-07 17:10:33 -0400

    commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('#title').fadeIn(4000);
+    $('#liltitle').fadeIn(4000);
+    $('#learn').fadeIn(4000);
+$('.contact').on('click', function(){
+  window.location = 'mailto:bhill7473@gmail.com';
+})
+
+
+
+})
